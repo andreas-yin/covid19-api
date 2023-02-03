b1dffc5923ad096e1dc23e3519df646659357016
Andreas Yin <andreas@Andreass-MacBook-Pro.local>
2023-02-03 23:07:28 +0100

    Add comment to router/other.js

--- routes/other.js
+++ routes/other.js
@@ -4,6 +4,7 @@ import invalidPath from '../controllers/other.js';
 
 const router = express.Router();
 
+// Handle requests to a path that is not available
 router.route('/').get(invalidPath).all(methodNotAllowed);
 
 export default router;
