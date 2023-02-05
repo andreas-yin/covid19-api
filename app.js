508aff4ecf0e234255179aa9dbf94de08fb5ac94
Andreas Yin <andreas@Andreass-MacBook-Pro.local>
2023-02-05 10:50:35 +0100

    Separate app from server, create handler.js file

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 import express from 'express';
-import serverless from 'serverless-http';
 import { apiRouter } from './routes/index.js';
 
-const app = express();
+export const app = express();
 
 app.use('/', apiRouter);
-
-export const server = serverless(app);
