3585e5a67caa1ea6465035e8b8cc2abc0249f69a
Andreas Yin <andreas@Andreass-MacBook-Pro.local>
2023-02-05 02:00:19 +0100

    Shorten comments

--- routes/countries.js
+++ routes/countries.js
@@ -7,14 +7,10 @@ import {
 
 export const countriesRouter = Router();
 
-// GET requests to retrieve summary of COVID cases for all countries and any requests other than GET
+// GET requests to retrieve COVID summary for all countries and any requests other than GET
 countriesRouter.route('/').get(getCountries).all(methodNotAllowed);
 
-/*
-GET requests to retrieve summary of COVID cases (or a single datapoint thereof) 
-for a specific country 
-and any requests other than GET
-*/
+// GET requests to retrieve COVID summary for a specific country and any requests other than GET
 countriesRouter.route('/:country').get(getCountry).all(methodNotAllowed);
 
 // If any error occurs during the above, return 500 HTTP error
